bbab70bb82339f0395bc4a46580ac68c9a6f3dc1
Daniel San <eduardo.daniel.s.m@gmail.com>
2019-10-14 15:22:17 -0400

    Added comments to functions in site.js file

--- EasyTagProject/wwwroot/js/site.js
+++ EasyTagProject/wwwroot/js/site.js
@@ -1,21 +1,27 @@
-﻿$(document).ready(addEventListener(
-    'click',
-    (e) => {
-        console.log('run')
-        if ($(e.target).hasClass('d-table-cell')) {
-            let displayClass = 'displayed';
-            let idNum = $(e.target).parent().attr('id').split('-')[0];
-            let tr = $('#' + idNum + '-header');
-            let hiddenId = '#' + idNum + "-description";
+﻿
+// Function to fade in and out the description
+let fadeAppointmentFunction = (e) => {
+    if ($(e.target).hasClass('d-table-cell')) {
+        let displayClass = 'displayed';
+        let idNum = $(e.target).parent().attr('id').split('-')[0];
+        let tr = $('#' + idNum + '-header');
+        let hiddenId = '#' + idNum + "-description";
 
-            if ($(tr).hasClass(displayClass)) {
-                $(tr).removeClass(displayClass);
-                $(hiddenId).fadeOut();
-            } else {
-                $(tr).addClass(displayClass);
-                $(hiddenId).fadeIn();
-            }
+        if ($(tr).hasClass(displayClass)) {
+            $(tr).removeClass(displayClass);
+            $(hiddenId).fadeOut();
+        } else {
+            $(tr).addClass(displayClass);
+            $(hiddenId).fadeIn();
         }
     }
-));
+};
+
+// load event listeners
+let addEventsListeners = function () {
+    $(document).click(fadeAppointmentFunction);
+};
+
+// add events after the page is loaded
+$(document).ready(addEventListener('load', addEventsListeners));
 
