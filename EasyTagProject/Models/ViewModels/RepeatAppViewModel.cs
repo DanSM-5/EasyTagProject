b04e279999870477a18b6d80b210f935863b3316
Daniel San <eduardo.daniel.s.m@gmail.com>
2020-02-26 15:54:07 -0500

    - UI adjustments

--- EasyTagProject/Models/ViewModels/RepeatAppViewModel.cs
+++ EasyTagProject/Models/ViewModels/RepeatAppViewModel.cs
@@ -26,7 +26,7 @@ namespace EasyTagProject.Models.ViewModels
         public bool IncludeWeekends { get; set; }
         [HiddenInput]
         public string Date { get; set; }
-        [RequiredIf("PeriodType == Once", ErrorMessage = "Please select a day!")]
+        [RequiredIf("PeriodType == RepeatTime.Once", ErrorMessage = "Please select a day!")]
         public DateTime? TargetDay { get; set; }
     }
 }
